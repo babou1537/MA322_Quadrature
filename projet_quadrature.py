1d51cdf762945faa41048cdde649783dfdec6302
Baptiste <129543701+babou1537@users.noreply.github.com>
2024-04-08 11:00:55 +0200

    Bienvenue 2
    blabla En tete du projet

---
+++ projet_quadrature.py
@@ -0,0 +1,11 @@
+""" MA322 - Quadrature & resolution numerique & d’EDO
+Projet : modelisation des syst ´ emes chaotiques par le pendule double 
+
+Membre du Groupe :
+    - Maëva AGUESSY
+    - Chloé TONSO
+    - Baptiste MATHIEU
+    - Phuong Mai NGUON
+
+Classe 3PSB1
+"""
